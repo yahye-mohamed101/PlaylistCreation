2a52fb93eb3c2c989cc587ab6ca6344b187d8ed9
EthanForrestCarr <ethan@ethancarr.com>
2024-09-30 18:53:43 -0500

    defined a few objects and set up our redirect page function.

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+// CONSTS
+
+const username = document.querySelector('#exampleInputEmail1');
+const password = document.querySelector('#exampleInputPassword1');
+const submitButton = document.querySelector('#submitButton');
+
+const existingUser = JSON.parse(localStorage.getItem('user')) || [];
+
+const user = {
+    username,
+    password
+};
+
+// LETS
+
+let redirectURL = ('index.html');
+
+// EVENT LISTENERS
+
+submitButton.addEventListener('click')
+
+// VARIABLES AS FUNCITONS
+
+const redirectPage = function (url) {
+  redirectURL = url;
+  location.assign(url);
+};
+
+// FUNCTIONS
+
+function storeSubmission(event) {
+    event.preventDefault();
+
+    if (!username || !password) {
+        document.querySelector(HEY YO THIS NEEDS TO SELECT SOMETHING).innerText = "Please enter a valid email and password.";
+        return;
+    } else {
+        localStorage.setItem('user', JSON.stringify(user));
+        redirectPage('userHome.html')
+    };
+}
