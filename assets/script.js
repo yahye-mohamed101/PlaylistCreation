c0b62b024fd109cf465d893ed77d07eeb125fe53
JoshAskew <asksprit3@gmail.com>
2024-10-01 14:11:14 -0500

    dark mode stuff

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,7 @@ const createButton = document.querySelector('#createPlaylist');
 const playlistAccordian = document.querySelector('#playlistAccordian');
 const body = document.body;
 const toggleButton = document.querySelector('.toggleButton');
+const h5 = document.querySelector('h5');
 
 const existingUser = JSON.parse(localStorage.getItem('user')) || [];
 
@@ -127,6 +128,7 @@ function toggleTheme() {
     isDarkMode = !isDarkMode;
 
     if (isDarkMode) {
+        h5.setAttribute('data-theme', 'dark');
         body.setAttribute('data-theme', 'dark');
         toggleButton.textContent = 'Switch to Light Mode';
         localStorage.setItem('theme', 'dark');
